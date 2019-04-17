47e8126b6f3188c2d7b46f913b6f208cff137573
zhuyanqing927 <zhu.yanqing927@126.com>
2019-04-17 17:44:50 +0800

    新增希尔排序

---
+++ src/main/java/com/yankie/zhu/algorithm/sort/ShellSort.java
@@ -0,0 +1,30 @@
+package com.yankie.zhu.algorithm.sort;
+
+import com.yankie.zhu.algorithm.utils.AlgorithmUtils;
+
+public class ShellSort {
+
+    public static void main(String[] args) {
+        int[] array = AlgorithmUtils.generateIntArray(20);
+
+        shellSort(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            System.out.print(" ");
+        }
+    }
+
+    private static void shellSort(int[] array) {
+        int len = array.length;
+
+        for (int h = len / 2; h > 0; h = h / 2) {
+            for (int i = h; i < len; i++) {
+                for (int j = i - h; j >= 0; j = j - h) {
+                    if (array[j] > array[j + h]) {
+                        AlgorithmUtils.swap(array, j, j + h);
+                    }
+                }
+            }
+        }
+    }
+}
